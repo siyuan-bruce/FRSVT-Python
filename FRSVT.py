bd9456f4cf81d3ff0a5dae821da44012f460b947
Jin Bruce <jinbruce@Jins-MacBook-Air.local>
2023-08-12 12:31:14 +0800

    result

--- FRSVT.py
+++ FRSVT.py
@@ -3,16 +3,11 @@ from numpy.linalg import norm
 import time
 
 def Polar(C):
-    print(C.shape)
     U, S, V = np.linalg.svd(C)
     W = np.dot(U, V.T)
-    print(W.shape)
-    print(V.shape)
-    print(S.shape)
     # transform S into a diagonal matrix
     S = np.diag(S)
     P = np.dot(np.dot(V, S), V.T)
-    print("P Shape", P.shape)
     return W, P
 
 def QR_CP(Y):
@@ -35,7 +30,6 @@ def QR_CP(Y):
         bmax2 = np.zeros((m, 1))
         flag2 = j
         
-        print(bmax2.shape)
 
         for i in range(j):
             
@@ -50,15 +44,8 @@ def QR_CP(Y):
             #for i in range(j):
             #bmax2 += np.outer(Q[:, i], np.squeeze(B[:, j]))
         
-        print(B.shape)
-        
-        print(bmax2.shape)
-        
-        print(B[:, j].shape)
-        
         bmax2 = np.reshape(B[:, j], (m,1)) - bmax2
         
-        print(bmax2.shape)
 
         for u in range(j + 1, n):
             c = np.zeros((m, 1))
@@ -96,10 +83,6 @@ def Helper(A, tau, l, p, Q):
     C = C[:C.shape[1], :]
     W, P = Polar(C)
     D, V = np.linalg.eig(P)
-    print("Qshape:", Q.shape)
-    print("Vshape", V.shape)
-    print("Dshape", D.shape)
-    print(D)
     D = np.diag(D)
     X = np.dot(np.dot(Q, V), S_tau(D, tau))
     X = np.dot(X, np.dot(np.dot(H, W), V).T)
@@ -144,13 +127,10 @@ def FRSVT(A, tau=None, l=None, p=None):
         A = A.T
 
     m, n = A.shape
-    print(A.shape)
     Omega = np.random.randn(n, l)
-    print(Omega.shape)
     Y = np.dot(A, Omega)
     print(Y.shape)
     Q = QR_CP(Y)
-    print(Q.shape)
     X, Q = Helper(A, tau, l, p, Q)
 
     Omega = np.random.randn(n, p)
@@ -160,6 +140,12 @@ def FRSVT(A, tau=None, l=None, p=None):
 
     toc = time.time()
     print("Elapsed Time:", toc - tic)
+
+    # save image
+    import matplotlib.pyplot as plt
+    plt.imshow(np.uint8(X), cmap='gray')
+    # save
+    plt.savefig('Q.jpg')
     
 
 import numpy as np
@@ -175,6 +161,9 @@ def TestSVD():
     tic = time.time()
 
     S, V, D = np.linalg.svd(I)
+
+    toc = time.time()
+    print("Elapsed Time:", toc - tic)
     plt.imshow(np.uint8(I), cmap='gray')
     plt.show()
 
@@ -183,12 +172,213 @@ def TestSVD():
     plt.imshow(np.uint8(reconstructed_image), cmap='gray')
     plt.show()
 
-    toc = time.time()
-    print("Elapsed Time:", toc - tic)
-
 # Call the test function
 TestSVD()
 
 A = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE).astype(float)
 
 FRSVT(A)
+
+
+import numpy as np
+import logging
+
+from sklearn.utils.extmath import randomized_svd, svd_flip
+from scipy.sparse.linalg import svds
+from numpy.linalg import norm
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import linalg as la
+import time
+import os
+
+
+def variance_scaled_ls_probs(m, n, A):
+    # populates array with the row-norms squared of matrix A
+    row_norms = np.zeros(m)
+    for i in range(m):
+        row_norms[i] = np.abs(la.norm(A[i, :]))**2
+
+    # Frobenius norm of A
+    A_Frobenius = np.sqrt(np.sum(row_norms))
+
+    LS_prob_rows = np.zeros(m)
+
+    # normalized length-square row probability distribution
+    for i in range(m):
+        LS_prob_rows[i] = row_norms[i] / A_Frobenius**2
+
+    LS_prob_columns = np.zeros((m, n))
+
+    # populates array with length-square column probability distributions
+    # LS_prob_columns[i]: LS probability distribution for selecting columns from row A[i]
+    for i in range(m):
+        LS_prob_columns[i, :] = [np.abs(k)**2 / row_norms[i] for k in A[i, :]]
+
+    # New part: compute variances and adjust the probabilities
+    row_vars = np.var(A, axis=1)  # row variances
+    col_vars = np.var(A, axis=0)  # column variances
+
+    row_adj = 1 / (1 + row_vars)  # adjust row probabilities - lower for high variance
+    col_adj = 1 / (1 + col_vars)  # adjust column probabilities - lower for high variance
+
+    LS_prob_rows *= row_adj  # adjust row probabilities
+    LS_prob_rows /= np.sum(LS_prob_rows)  # renormalize
+
+    for i in range(m):
+        LS_prob_columns[i, :] *= col_adj  # adjust column probabilities
+        LS_prob_columns[i, :] /= np.sum(LS_prob_columns[i, :])  # renormalize each row
+
+    return row_norms, LS_prob_rows, LS_prob_columns, A_Frobenius
+
+
+
+def ls_probs(m, n, A):
+
+    r"""Function generating the length-squared (LS) probability distributions for sampling matrix A.
+
+    Args:
+        m (int): number of rows of matrix A
+        n (int): row n of columns of matrix A
+        A (array[complex]): most general case is a rectangular complex matrix
+
+    Returns:
+        tuple: Tuple containing the row-norms, LS probability distributions for rows and columns,
+        and Frobenius norm.
+    """
+
+    # populates array with the row-norms squared of matrix A
+    row_norms = np.zeros(m)
+    for i in range(m):
+        row_norms[i] = np.abs(la.norm(A[i, :]))**2
+
+    # Frobenius norm of A
+    A_Frobenius = np.sqrt(np.sum(row_norms))
+
+    LS_prob_rows = np.zeros(m)
+
+    # normalized length-square row probability distribution
+    for i in range(m):
+        LS_prob_rows[i] = row_norms[i] / A_Frobenius**2
+
+    LS_prob_columns = np.zeros((m, n))
+
+    # populates array with length-square column probability distributions
+    # LS_prob_columns[i]: LS probability distribution for selecting columns from row A[i]
+    for i in range(m):
+        LS_prob_columns[i, :] = [np.abs(k)**2 / row_norms[i] for k in A[i, :]]
+
+    return row_norms, LS_prob_rows, LS_prob_columns, A_Frobenius
+
+
+def sample_C(A, m, n, r, c, row_norms, LS_prob_rows, LS_prob_columns, A_Frobenius):
+
+    r"""Function used to generate matrix C by performing LS sampling of rows and columns of matrix A.
+
+    Args:
+        A (array[complex]): rectangular, in general, complex matrix
+        m (int): number of rows of matrix A
+        n (int): number of columns of matrix A
+        r (int): number of sampled rows
+        c (int): number of sampled columns
+        row_norms (array[float]): norm of the rows of matrix A
+        LS_prob_rows (array[float]): row LS probability distribution of matrix A
+        LS_prob_columns (array[float]): column LS probability distribution of matrix A
+        A_Frobenius (float): Frobenius norm of matrix A
+
+    Returns:
+        tuple: Tuple containing the singular values (sigma), left- (w) and right-singular vectors (vh) of matrix C,
+        the sampled rows (rows), the column LS prob. distribution (LS_prob_columns_R) of matrix R and split running
+        times for the FKV algorithm.
+    """
+
+    tic = time.time()
+    # sample row indices from row length_square distribution
+    rows = np.random.choice(m, r, replace=True, p=LS_prob_rows)
+
+    columns = np.zeros(c, dtype=int)
+    # sample column indices
+    for j in range(c):
+        # sample row index uniformly at random
+        i = np.random.choice(rows, replace=True)
+        # sample column from length-square distribution of row A[i]
+        columns[j] = np.random.choice(n, 1, p=LS_prob_columns[i])
+
+    toc = time.time()
+    rt_sampling_C = toc - tic
+
+    # building the lenght-squared distribution to sample columns from matrix R
+    R_row = np.zeros(n)
+    LS_prob_columns_R = np.zeros((r, n))
+
+    tic = time.time()
+    # creates empty array for R and C matrices. We treat R as r x c here, since we only need columns later
+    R_C = np.zeros((r, c))
+    C = np.zeros((r, c))
+
+    # populates array for matrix R with the submatrix of A defined by sampled rows/columns
+    for s in range(r):
+        for t in range(c):
+            R_C[s, t] = A[rows[s], columns[t]]
+
+        # renormalize each row of R
+        R_C[s,:] = R_C[s,:] * A_Frobenius / (np.sqrt(r) * np.sqrt(row_norms[rows[s]]))
+
+    # creates empty array of column norms
+    column_norms = np.zeros(c)
+
+    # computes column Euclidean norms
+    for t in range(c):
+        for s in range(r):
+            column_norms[t] += np.abs(R_C[s, t])**2
+
+    # renormalize columns of C
+    for t in range(c):
+        C[:, t] = R_C[:, t] * (A_Frobenius / np.sqrt(column_norms[t])) / np.sqrt(c)
+
+    return C, rows, columns
+
+
+def quantum_inspired_FRSVT(A, tau=None, l=None, p=None):
+    import time
+    
+    if A is None:
+        # Read an image if A is not provided
+        import cv2
+        A = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE).astype(float)
+
+    if tau is None:
+        tau = 1 / np.linalg.norm(A, 2)
+    if l is None:
+        l = 20
+    if p is None:
+        p = 10
+
+    if A.shape[0] < A.shape[1]:
+        A = A.T
+
+    row_norms, LS_prob_rows, LS_prob_columns, A_Frobenius =  ls_probs(A.shape[0], A.shape[1], A)
+
+    tic = time.time()
+
+    m, n = A.shape
+    Y, rows, columns = sample_C(A, A.shape[0], A.shape[1], m, l, row_norms, LS_prob_rows, LS_prob_columns, A_Frobenius)
+    print(Y.shape)
+    Q = QR_CP(Y)
+    X, Q = Helper(A, tau, l, p, Q)
+
+    Y, rows, columns = sample_C(A, A.shape[0], A.shape[1], m, p, row_norms, LS_prob_rows, LS_prob_columns, A_Frobenius)
+    Q = PartialOrthogonalization(Q, Y)
+    X, Q = Helper(A, tau, l, p, Q)
+
+    toc = time.time()
+    print("Elapsed Time:", toc - tic)
+
+    # save image
+    import matplotlib.pyplot as plt
+    plt.imshow(np.uint8(X), cmap='gray')
+    # save
+    plt.savefig('Inspired_Q.jpg')
+    
+quantum_inspired_FRSVT(A)
